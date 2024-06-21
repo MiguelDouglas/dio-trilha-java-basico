146a22a542ca89535444e7d6c89f3bddcac7e78c
MiguelDouglas <miguel.douglas.silva@gmail.com>
2024-06-21 06:03:44 -0300

    início do código

--- Conta-Banco/src/ContaTerminal.java
+++ Conta-Banco/src/ContaTerminal.java
@@ -1,5 +1,15 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        
+        Scanner scanner = new Scanner (System.in).useLocale(Locale.US)
+
+        System.out.print("")
+    
+    
+    
+    }   
 }
